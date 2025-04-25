67422af65b552c3e172dd6d1fe392932a8697dec
Wahyu Wijanarko <wahyuwijanarko3998@gmail.com>
2025-04-25 23:52:21 +0700

    Init project

---
+++ src/main/java/com/wecode/jpa_spec/JpaSpecApplication.java
@@ -0,0 +1,13 @@
+package com.wecode.jpa_spec;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpaSpecApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JpaSpecApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/wecode/jpa_spec/JpaSpecApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wecode.jpa_spec;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpaSpecApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
